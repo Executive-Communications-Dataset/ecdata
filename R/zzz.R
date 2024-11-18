4a225b0b41654fe2834f7bf7a70260cfc715dec0
desktop <jallen108@gsu.edu>
2024-11-18 10:14:17 -0500

    try fixing cacheing message

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,7 @@
 
 .onLoad = function(libname,pkgname){
 
-  rlang::check_installed(
-    pkg = "arrow (>= 16.1.0)",
-    reason = "To read in the the ECD data files we need to install arrow."
-  )
+
 
   ## this is a direct rip of the nflreadr cachemethod found 
   ## here https://github.com/nflverse/nflreadr/blob/main/R/zzz.R
